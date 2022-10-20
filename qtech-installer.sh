0fddce41e5c1c8a219fd1a1443a672b429b9f196
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-20 22:00:13 +0500

    Update qtech-installer.sh

--- qtech-installer.sh
+++ qtech-installer.sh
@@ -3,15 +3,16 @@
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose keepalived -y
 sudo systemctl enable --now docker
+# Wimark
+wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
+unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
 # Navigator
 curl -sSL https://repo.45drives.com/setup | sudo bash
 sudo dnf install cockpit-navigator -y
 # ssh
 ssh-keygen
 cat ~/.ssh/id_rsa.pub | ssh user@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" 
-wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
-unzip QWC-VC-1.8.1-19-08-2022-installer.zip
-sudo https://raw.githubusercontent.com/32fca06c/qtech/main/master-backup.sh -o /etc/wimark/master-backup.sh
+sudo wget https://raw.githubusercontent.com/32fca06c/qtech/main/master-backup.sh -o /etc/wimark/master-backup.sh
 sudo chmod ug+x /etc/wimark/master-backup.sh
 # Keepalived
 sudo systemctl enable --now keepalived
