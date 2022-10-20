3de7248078b2a7fee72cfbe4f37df40bc6194e49
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 00:51:33 +0500

    Update and rename qtech-installer.sh to qtech-standalone.sh

--- qtech-standalone.sh
+++ qtech-standalone.sh
@@ -3,12 +3,13 @@
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose keepalived -y
 sudo systemctl enable --now docker
-# Wimark
-wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
-unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
 # Navigator
 curl -sSL https://repo.45drives.com/setup | sudo bash
 sudo dnf install cockpit-navigator -y
+# Wimark
+wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
+unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
+rm QWC-VC-1.8.1-19-08-2022-installer.zip
 # ssh
 ssh-keygen
 cat ~/.ssh/id_rsa.pub | ssh user@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" 
