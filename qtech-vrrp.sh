92a0cbc32bcac79a69ed7d390a2e3d77cdd72756
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 01:08:18 +0500

    Update qtech-vrrp.sh

--- qtech-vrrp.sh
+++ qtech-vrrp.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Detect public IPv4 or IPv6 address and pre-fill for the user
+	SERVER_PUB_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
+	read -rp "IPv4: " -e -i "${SERVER_PUB_IP}" SERVER_PUB_IP
+
+	# Detect public interface and pre-fill for the user
+	SERVER_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
+	until [[ ${SERVER_PUB_NIC} =~ ^[a-zA-Z0-9_]+$ ]]; do
+		read -rp "Public interface: " -e -i "${SERVER_NIC}" SERVER_PUB_NIC
+	done
+  
 # Docker
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose keepalived -y
@@ -12,7 +23,7 @@ unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
 rm QWC-VC-1.8.1-19-08-2022-installer.zip
 # ssh
 ssh-keygen
-cat ~/.ssh/id_rsa.pub | ssh user@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" 
+cat ~/.ssh/id_rsa.pub | ssh user@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 sudo wget https://raw.githubusercontent.com/32fca06c/qtech/main/master-backup.sh -o /etc/wimark/master-backup.sh
 sudo chmod ug+x /etc/wimark/master-backup.sh
 # Keepalived
