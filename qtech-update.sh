95f3b8ba2d610ad37c02e9539169137f9857f6dc
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 01:02:53 +0500

    Update qtech-update.sh

--- qtech-update.sh
+++ qtech-update.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker container rm $(sudo docker ps -a -q)
+wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
+unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
+rm QWC-VC-1.8.1-19-08-2022-installer.zip
+sudo bash QWC-VC-1.8.1-19-08-2022-installer.run
