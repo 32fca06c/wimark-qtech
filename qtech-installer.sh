57020a2be5a0955bcc94c3117f38add8c3d5cfea
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-20 21:08:38 +0500

    Update qtech-installer.sh

--- qtech-installer.sh
+++ qtech-installer.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# Docker
+sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io -y
+sudo dnf install docker-compose -y
+sudo systemctl enable --now docker
+# Keepalived
+sudo dnf install keepalived -y
+# ssh
+ssh-keygen
+cat ~/.ssh/id_rsa.pub | ssh user@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" 
+wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
