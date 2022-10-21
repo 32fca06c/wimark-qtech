1273da62e844f0541f52da8f3397355dd52e8092
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 11:41:39 +0500

    Update qtech-standalone.sh

--- qtech-standalone.sh
+++ qtech-standalone.sh
@@ -4,8 +4,7 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/dock
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo systemctl enable --now docker
 # Wimark
-wget https://files.qtech.ru/upload/wireless/QWC-VC/QWC-VC-1.8.1-19-08-2022-installer.zip
+wget $(wget -nv -O- https://www.qtech.ru/catalog/wireless/lan_controllers/qwc_wm/#documentation | grep -Po 'https:\/\/files\.qtech\.ru\/upload\/wireless\/QWC-VC\/QWC-VC-[\d\.]+-\d{2}-\d{2}-\d{4}-installer\.zip')
 unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
 rm QWC-VC-1.8.1-19-08-2022-installer.zip
 sudo bash QWC-VC-1.8.1-19-08-2022-installer.run
-# pcsync-https-https-http-mqtt-openvpn-isakmp-ipsec-nat-t
