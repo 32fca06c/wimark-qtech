06f36052aa169321b4a6ee4fe285a0dd36421c70
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 12:41:56 +0500

    Update qtech-vrrp.sh

--- qtech-vrrp.sh
+++ qtech-vrrp.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Detect public IPv4 or IPv6 address and pre-fill for the user
-	SERVER_PUB_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
-	read -rp "Primary (current) WLC IPv4 address: " -e -i "${SERVER_PUB_IP}" SERVER_PUB_IP
-
-	# Detect public interface and pre-fill for the user
-	SERVER_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
-	until [[ ${SERVER_PUB_NIC} =~ ^[a-zA-Z0-9_]+$ ]]; do
-		read -rp "Public interface: " -e -i "${SERVER_NIC}" SERVER_PUB_NIC
-	done
-  
+PRIMARY_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
+read -rp "Primary (current) WLC IPv4 address: " -e -i "${PRIMARY_IP}" PRIMARY_IP
+read -rp "Secondary WLC IPv4 address: " -e -i "${SECONDARY_IP}" SECONDARY_IP
+	
+SERVER_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
+until [[ ${SERVER_NIC} =~ ^[a-zA-Z0-9_]+$ ]]; do
+	read -rp "Public interface: " -e -i "${SERVER_NIC}" SERVER_NIC
+done
+ 
 # Docker
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose keepalived -y
@@ -22,9 +21,10 @@ URL=$(wget -nv -O- https://www.qtech.ru/catalog/wireless/lan_controllers/qwc_wm/
 wget -O QWC-VC-HA-installer.zip $URL
 unzip -q QWC-VC-HA-installer.zip
 sudo bash QWC-VC-*-installer.run --target /home/$(whoami)/QWC-VC-HA-installer
+ssh $(whoami)@$SECONDARY_IP
 # ssh
 ssh-keygen
-cat ~/.ssh/id_rsa.pub | ssh $(whoami)@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh $(whoami)@$SECONDARY_IP "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 sudo wget https://raw.githubusercontent.com/32fca06c/qtech/main/master-backup.sh -o /etc/wimark/master-backup.sh
 sudo chmod ug+x /etc/wimark/master-backup.sh
 # Keepalived
