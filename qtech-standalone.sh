afc5e35886db1be6deba1d80a7240d2a77203865
adner <67954372+32fca06c@users.noreply.github.com>
2022-10-21 11:49:45 +0500

    Update qtech-standalone.sh

--- qtech-standalone.sh
+++ qtech-standalone.sh
@@ -7,4 +7,4 @@ sudo systemctl enable --now docker
 wget $(wget -nv -O- https://www.qtech.ru/catalog/wireless/lan_controllers/qwc_wm/#documentation | grep -Po 'https:\/\/files\.qtech\.ru\/upload\/wireless\/QWC-VC\/QWC-VC-[\d\.]+-\d{2}-\d{2}-\d{4}-installer\.zip')
 unzip -q QWC-VC-1.8.1-19-08-2022-installer.zip
 rm QWC-VC-1.8.1-19-08-2022-installer.zip
-sudo bash QWC-VC-1.8.1-19-08-2022-installer.run
+sudo bash QWC-VC-1.8.1-19-08-2022-installer.run --target /home/$(whoami)/tmp
